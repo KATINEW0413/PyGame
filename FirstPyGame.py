e5cdb1bf62c6273e4afbb68d56183a5218da9b00
KATINEW <katinew0413@gmail.com>
2023-06-11 03:02:53 +0900

    Create FirstPyGame.py

---
+++ FirstPyGame.py
@@ -0,0 +1,74 @@
+import pygame
+
+pygame.init()
+
+background = pygame.display.set_mode((1600, 900))
+pygame.display.set_caption("PyGame made by MJ")
+
+fps = pygame.time.Clock()
+
+x_pos = background.get_size()[0]//2
+y_pos = background.get_size()[1]//2
+
+up_go = False
+down_go = False
+right_go = False
+left_go = False
+
+to_x = 0
+to_y = 0
+
+move = 4
+
+play = True
+
+while play:
+    deltaTime = fps.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                up_go = True
+                print("UP")
+            if event.key == pygame.K_DOWN:
+                down_go = True
+                print("DOWN")
+            if event.key == pygame.K_RIGHT:
+                right_go = True
+                print("RIGHT")
+            if event.key == pygame.K_LEFT:
+                left_go = True
+                print("LEFT")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                up_go = False
+            if event.key == pygame.K_DOWN:
+                down_go = False
+            if event.key == pygame.K_RIGHT:
+                right_go = False
+            if event.key == pygame.K_LEFT:
+                left_go = False
+
+    if up_go == True:
+        y_pos -= move
+        if y_pos <= 0:
+            y_pos = 0
+    if down_go == True:
+        y_pos += move
+        if y_pos >= 900:
+            y_pos = 900
+    if right_go == True:
+        x_pos += move
+        if x_pos >= 1600:
+            x_pos = 1600
+    if left_go == True:
+        x_pos -= move
+        if x_pos <= 0:
+            x_pos = 0
+
+    background.fill((0, 0, 0))
+    pygame.draw.circle(background, (255, 0, 0), (x_pos, y_pos), 15)            
+    pygame.display.update()
+
+pygame.quit()
